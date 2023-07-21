682adf59b5b31b997d8e480dbc6d26c3b30f17fb
Thanusuyasri <82963993+Thanusuyasri@users.noreply.github.com>
2023-07-21 10:58:35 +0530

    first commit

--- src/Printfrom1toA.java
+++ src/Printfrom1toA.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Printfrom1toA {
+import java.util.Scanner;
+
+public class Printfrom1toA {
+    public static int printnaturalnum(int n)
+    {
+        if(n==1)
+        {
+            System.out.println(1+" ");
+            return 1;
+        }
+        else {
+            printnaturalnum(n-1);
+            System.out.println(n);
+            return (n);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        printnaturalnum(n);
+    }
 }
